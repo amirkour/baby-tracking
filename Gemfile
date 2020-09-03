36310986aa33c118c906fca9f903b44775fdd71a
Amir Kouretchian <amir.kouretchian@gmail.com>
2020-09-03 13:38:48 -0700

    reloader working

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 ruby '>= 2.6.0', '<= 2.7'
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'haml'
--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
 require 'haml'
+require "sinatra/reloader"
 
 set :haml, :format => :html5
+
+configure :production do
+    enable :reloader
+end
+
+get '/feed/:id' do
+    haml :edit_feed
+end
 
 get '/feed' do
     haml :feed
