94772965b838c111daf5a48f4d43ff74307350ce
Amir Kouretchian <amir.kouretchian@gmail.com>
2020-09-03 17:29:29 -0700

    adding logging and dev/prod differences. gonna test on heroku in a sec

--- app.rb
+++ app.rb
@@ -1,11 +1,26 @@
 require 'sinatra'
 require 'haml'
 require "sinatra/reloader"
+require 'sinatra/custom_logger'
+require 'logger'
 
 set :haml, :format => :html5
 
-configure :production do
+
+# TODO - edit connection string here ..?
+# TODO - log-file settings, like rolling and size restrictions ..?
+configure :development do
+    set :logger, Logger.new('./bin/logfile.log')
+    settings.logger.level = Logger::DEBUG
+    settings.logger.debug("Woah, debug enabled!?")
     enable :reloader
+end
+configure :production do
+    set :logger, Logger.new(STDOUT)
+
+    # TODO - change this in actual prod - i'm testing for now
+    settings.logger.level = Logger::DEBUG
+    settings.logger.error("Woah, prod enabled!?")
 end
 
 get '/feed/:id' do
@@ -17,5 +32,8 @@ get '/feed' do
 end
 
 get '/' do
+    logger.info 'some message here'
+    logger.error 'some error'
+    logger.warn "loggers class is #{logger.class.name}"
     haml :index
 end
