8e5f70a831298178502f85c1f7da8a59b049dbac
Amir Kouretchian <amir.kouretchian@gmail.com>
2020-09-05 13:18:33 -0700

    config.ru, updated Procfile, enable logging to stdout for herok

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require 'logger'
 
 set :haml, :format => :html5
 
+# TODO - the web server prolly shouldn't be webrick out on prod ...
+# see this article: https://devcenter.heroku.com/articles/ruby-default-web-server
 
 # TODO - edit connection string here ..?
 # TODO - log-file settings, like rolling and size restrictions ..?
@@ -19,14 +21,19 @@ configure :production do
     set :logger, Logger.new(STDOUT)
 
     # TODO - change this in actual prod - i'm testing for now
-    settings.logger.level = Logger::DEBUG
+    settings.logger.level = Logger::WARN
     settings.logger.error("Woah, prod enabled!?")
 end
 
+
+# TODO - these routes should probably be something
+# like /baby/:id/feed/:id
 get '/feed/:id' do
     haml :edit_feed
 end
 
+# TODO - these routes should probably be something
+# like /baby/:id/feed/:id
 get '/feed' do
     haml :feed
 end
@@ -34,6 +41,6 @@ end
 get '/' do
     logger.info 'some message here'
     logger.error 'some error'
-    logger.warn "loggers class is #{logger.class.name}"
+    logger.warn "WARNING: loggers class is #{logger.class.name}"
     haml :index
 end
